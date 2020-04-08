ccc1bb5d3d6a052c886844d0b1df8018ad4b88c4
Christian Sporer <IanStorm28@outlook.com>
2020-04-08 15:54:40 +0200

    Fix broken logging configuration

--- settings.js
+++ settings.js
@@ -2,5 +2,11 @@ module.exports = {
 	credentialSecret: process.env.NODE_RED_CREDENTIAL_SECRET,
 	disableEditor: true,
 	flowFilePretty: true,
-	logging: "warn",
+	logging: {
+		console: {
+			level: "warn",
+			metrics: false,
+			audit: false,
+		},
+	},
 }
